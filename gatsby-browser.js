a58a6ef2c59e57ef7762848541a069da3dead3dd
Rafi Priatna K <rafipriatnak@gmail.com>
2021-02-08 23:11:06 +0700

    Added Prismjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,4 @@
 import './src/styles/global.css';
+require("prismjs/themes/prism-tomorrow.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
