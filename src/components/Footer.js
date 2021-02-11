096523af9b42e00370fc5ff2c53fec5ceeeb6f02
Rafi Priatna K <rafipriatnak@gmail.com>
2021-02-11 18:57:13 +0700

    Remove footer hover

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="footer relative my-10">
       <div className="text-sm text-white font-bold">
-        <div className="transition duration-200 ease-in-out hover:bg-gray-700 hover:text-white rounded-md px-2 py-2 lg:mt-4 mt-2">
+        <div className="transition duration-200 ease-in-out rounded-md px-2 py-2 lg:mt-4 mt-2">
           <div className="lg:flex flex-col lg:flex-row justify-between w-full lg:py-0">
             <div className="flex flex-col lg:flex-row">
               © 2021 Rafi Priatna K.
