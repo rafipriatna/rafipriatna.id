bcbc401fc7dace685c9095d3ec75b5e1af688245
Rafi Priatna K <rafipriatnak@gmail.com>
2021-02-11 19:04:57 +0700

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,25 +60,36 @@ export default function Home({ data, pageContext }) {
                 Artikel terbaru
             </h1>
 
-            {posts.map(post => {
-                const { frontmatter, fields, id } = post
-                const { title, date } = frontmatter
+            <div className="mb-6">
+                {posts.map(post => {
+                    const { frontmatter, fields, id } = post
+                    const { title, date } = frontmatter
 
-                return (
-                    <Link to={fields.slug} key={id}>
-                        <div className="transition duration-200 ease-in-out hover:bg-gray-700 hover:text-white rounded-md px-2 py-2 lg:mt-4 mt-2">
-                            <div className="lg:flex flex-col lg:flex-row justify-between w-full lg:py-0">
-                                <div className="text-lg flex flex-col lg:flex-row">
-                                    {title}
-                                </div>
-                                <div className="text-md flex flex-col lg:flex-row lg:block hidden">
-                                    <span>{date}</span>
+                    return (
+                        <Link to={fields.slug} key={id}>
+                            <div className="transition duration-200 ease-in-out hover:bg-gray-700 hover:text-white rounded-md px-2 py-2 lg:mt-4 mt-2">
+                                <div className="lg:flex flex-col lg:flex-row justify-between w-full lg:py-0">
+                                    <div className="text-lg flex flex-col lg:flex-row">
+                                        {title}
+                                    </div>
+                                    <div className="text-md flex flex-col lg:flex-row lg:block hidden">
+                                        <span>{date}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Link>
-                )
-            })}
+                        </Link>
+                    )
+                })}
+            </div>
+
+            <div className="text-right">
+                <Link to="/blog">
+                    <span className="bg-gray-700 text-white rounded-md px-1 py-1">
+                        Lihat artikel lainnya
+                </span>
+                </Link>
+            </div>
+
         </Layout>
     )
 }
