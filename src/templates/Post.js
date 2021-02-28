46d12fdd4b640a499f18484d7d4aeeecd457591e
Rafi Priatna K <rafipriatnak@gmail.com>
2021-02-28 17:19:45 +0700

    Improve post header color

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -19,21 +19,23 @@ export default function Post({ data, pageContext }) {
     <Layout>
       <SEO post={dataPost} />
       <article itemScope itemType="http://schema.org/Article" className="px-2">
-        <header className="text-gray-300 mb-10 break-words">
+        <header className="mb-10 break-words">
           <h1 className="text-4xl mb-6">{title}</h1>
-          <p className="my-2">
-            Terbit pada tanggal {date}{" "}
-          </p>
+          <div className="text-gray-400">
+            <p className="my-2">
+              Terbit pada tanggal {date}{" "}
+            </p>
 
-          <p>
-            {tags.map(tag => {
-              return (
-                <Link to={"/tags/" + tag} className="font-bold pb-4 mr-2" key={tag}>
-                  #{tag}
-                </Link>
-              )
-            })}
-          </p>
+            <p>
+              {tags.map(tag => {
+                return (
+                  <Link to={"/tags/" + tag} className="font-bold pb-4 mr-2 hover:text-white" key={tag}>
+                    #{tag}
+                  </Link>
+                )
+              })}
+            </p>
+          </div>
         </header>
         <div className="prose prose-dark max-w-full break-words text-lg">
           <section
