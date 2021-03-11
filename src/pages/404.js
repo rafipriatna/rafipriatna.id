f0fa21f47c234f70076f2517378b27c37fdb7888
Rafi Priatna K <rafipriatnak@gmail.com>
2021-03-11 23:05:03 +0700

    Add props

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,14 @@ import SEO from "../components/Seo"
 
 export default function NotFound() {
 
+    const data = {
+        title: "Not Found :(",
+        description: "404 Not Found.",
+    }
+
     return (
         <Layout>
-            <SEO />
+            <SEO post={data} />
             <div className="px-2 text-lg">
                 <h1 className="text-4xl mb-10">404 :(</h1>
                 <p>Halaman yang kamu cari nggak ketemu nih...</p>
