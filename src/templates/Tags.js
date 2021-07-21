e22cd4ead9eafb51fc631b85ddb9e172a5f9c6d9
Rafi Priatna K <rafipriatnak@gmail.com>
2021-07-21 17:06:28 +0700

    Fix url from tagged post

--- src/templates/Tags.js
+++ src/templates/Tags.js
@@ -26,7 +26,7 @@ const Tags = ({ pageContext, data }) => {
         const { slug } = node.fields
         const { title, date } = node.frontmatter
         return (
-          <Link to={slug} key={node.id}>
+          <Link to={'/' + slug} key={node.id}>
             <div className="transition duration-200 ease-in-out hover:bg-gray-800 hover:text-gray-100 rounded-md px-3 py-2 lg:mt-4 mt-2">
               <div className="lg:flex flex-col lg:flex-row justify-between w-full lg:py-0">
                 <div className="text-lg flex flex-col lg:flex-row">
