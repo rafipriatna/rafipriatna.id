eff70b21156f2f65377d41072ba0a65a161e442a
Rafi Priatna K <rafipriatnak@gmail.com>
2021-08-20 18:01:37 +0700

    Added anchor offset

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import "./src/styles/global.css"
 import "prismjs/themes/prism-tomorrow.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 import "prismjs/plugins/command-line/prism-command-line.css"
-import "./src/styles/highlight.css"
+import "./src/styles/styles.css"
 
 const onInitialClientRender = () => {
   const theme = typeof window !== 'undefined' && localStorage.getItem("theme")
