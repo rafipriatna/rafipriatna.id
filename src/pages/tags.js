a5114334ccb3c6e13c7c55ff88a7c0d1c36534e3
Rafi Priatna K <rafipriatnak@gmail.com>
2021-10-03 12:03:09 +0700

    Update tags color

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -30,7 +30,9 @@ const TagsPage = ({
       {group.map(tag => (
         <div className="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/4 xl:w-1/4">
           <Link to={`/tags/${kebabCase(tag.fieldValue)}/`} key={tag.fieldValue}>
-            <button className="h-10 px-5 text-gray-100 transition-colors duration-150 bg-gray-800 rounded-lg focus:shadow-outline hover:bg-gray-800">
+            <button className="mr-2 px-2 py-1 border-2 my-2 h-10 px-5
+                    border-gray-400 bg-gray-200 hover:border-blue-600 hover:text-blue-600 hover:bg-blue-100
+                    dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-blue-600 dark:hover:text-blue-300 dark:hover:bg-blue-900">
               <span className="mr-2">{tag.fieldValue}</span>
               <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-gray-100 bg-gray-600 rounded-full">({tag.totalCount})</span>
             </button>
--- src/templates/Post.js
+++ src/templates/Post.js
@@ -31,7 +31,7 @@ export default function Post({ data, pageContext }) {
                     to={"/tags/" + tag}
                     className="mr-2 px-2 py-1 border-2 my-2
                     border-gray-400 bg-gray-200 hover:border-blue-600 hover:text-blue-600 hover:bg-blue-100
-                    dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-blue-600 dark:hover:text-blue-600"
+                    dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-blue-600 dark:hover:text-blue-300 dark:hover:bg-blue-900"
                     key={tag}
                   >
                     {tag}
