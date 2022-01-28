6cfeb6d9644ac3ad8e8e782e66e55e18861f0484
Rafi Priatna K <rafipriatnak@gmail.com>
2022-01-29 00:55:57 +0700

    Add Notion

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
+require("dotenv").config({
+  path: `.env`,
+});
+
 module.exports = {
   siteMetadata: {
-      title: `RafiPriatna.ID`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `RafiPriatna.ID`,
+    siteUrl: `https://rafipriatna.id`
   },
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
@@ -10,5 +14,14 @@ module.exports = {
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+    {
+      resolve: `gatsby-source-notion-api`,
+      options: {
+        token: process.env.NOTION_SECRET,
+        databaseId: process.env.DATABASE_ID,
+        propsToFrontmatter: true,
+        lowerTitleLevel: true,
+      },
+    },]
 };
