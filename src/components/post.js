57f8110de883defc84297ee87db06977b78d2062
Rafi Priatna K <rafipriatnak@gmail.com>
2022-02-06 00:32:43 +0700

    Crawling notion post

--- src/components/post.js
+++ src/components/post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-export function posts({ data, showYears }) {
+export default function Posts({ data, showYears }) {
 
     const postsByYear = {}
 
@@ -18,7 +18,7 @@ export function posts({ data, showYears }) {
             <section key={year}>
                 <h2 className='text-3xl mb-6 mt-10 px-2'>{year}</h2>
                 {postsByYear[year].map((node) => (
-                    <PostCell key={node.id} node={node} />
+                    <PostItem key={node.id} node={node} />
                 ))}
             </section>
         ))
@@ -26,20 +26,36 @@ export function posts({ data, showYears }) {
         return (
             <div>
                 {data.map((node) => (
-                    <PostCell key={node.id} node={node} />
+                    <PostItem key={node.id} node={node} />
                 ))}
             </div>
         )
     }
 }
 
-const postItem = ({ node }) => {
+const PostItem = ({ node }) => {
+
+    const type = node.icon.type
+    let thumbnail = ''
+    if (type == 'emoji') {
+        thumbnail = <span role='img'>{node.icon.emoji}</span>
+    } else if (type == 'external') {
+        thumbnail = <img src={node.icon.external.url} className='w-7 h-7 rounded' />
+    } else {
+        thumbnail = <span role='img'>📝</span>
+    }
+
     return (
         <Link to={node.slug} key={node.id}>
             <div className='transition duration-200 ease-in-out hover:bg-gray-700 hover:text-white rounded-md px-2 py-2 lg:mt-4 mt-2'>
-                <div className='lg:flex flex-col lg:flex-row justify-between w-full lg:py-0'>
-                    <div className='text-lg flex flex-col lg:flex-row'>
-                        {node.title}
+                <div className='lg:flex inline-block lg:flex-row justify-between w-full lg:py-0'>
+                    <div className='text-lg flex inline-block lg:flex-row'>
+                        <div className='max-w-xs mr-2'>
+                            {thumbnail}
+                        </div>
+                        <h2 >
+                            {node.title}
+                        </h2>
                     </div>
                     <div className='text-md flex flex-col lg:flex-row lg:block hidden'>
                         <span>{node.date}</span>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,80 @@
-import * as React from 'react'
+import React, { useMemo } from 'react'
+import { graphql } from 'gatsby'
 
 // Layout
 import Layout from '../layouts/default'
 
 // Components
 import Hero from '../components/hero'
+import Posts from '../components/post'
+
+// Helper
+import { notionPostFormat } from '../lib/notion-post-format'
 
 const IndexPage = ({ data, pageContext }) => {
+  const { allNotion } = data
+
+  const notionPost = useMemo(
+    () => notionPostFormat(allNotion.edges),
+    [allNotion.edges]
+  )
+
   return (
     <Layout>
       <Hero />
 
       <h1 className="text-3xl my-10 pt-10 font-semibold">Artikel terbaru</h1>
+      <div className="mb-6">
+        <Posts data={notionPost} />
+      </div>
     </Layout>
   )
 }
+
+export const indexQuery = graphql`
+query IndexQuery {
+  allNotion(
+    filter: {properties: {type: {value: {name: {eq: "Article"}}}}}
+    limit: 5
+  ) {
+    edges {
+      node {
+        id
+        raw {
+          icon {
+            type
+            external {
+              url
+            }
+            emoji
+          }
+          properties {
+            Name {
+              id
+            }
+            date {
+              date {
+                start(locale: "id-ID", formatString: "DD MMMM YYYY")
+              }
+            }
+            slug {
+              rich_text
+            }
+            tags {
+              multi_select {
+                name
+              }
+            }
+            description {
+              rich_text
+            }
+          }
+        }
+        title
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
