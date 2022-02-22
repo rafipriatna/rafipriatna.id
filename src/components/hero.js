02bb2e58ec0c805b78684d5fba081188900a6ee1
Rafi Priatna K <rafipriatnak@gmail.com>
2022-02-22 23:08:44 +0700

    update home page

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,9 +13,6 @@ export default function Hero() {
                     <span className='animate-pulse text-indigo-500'>Rafi</span>.
                     Selamat datang di kebun digital saya {' '} <span role='img' aria-label='tanaman'>🌱</span>.
                 </h1>
-                <Link to='/tentang' className='bg-indigo-500 text-white text-lg px-4 py-2 transition duration-300 ease-in-out hover:bg-indigo-600 mr-6 shadow-lg shadow-indigo-500/50 dark:shadow-indigo-800/50 rounded-lg'>
-                    Tentang
-                </Link>
                 <Link to='/blog' className='bg-indigo-500 text-white text-lg px-4 py-2 transition duration-300 ease-in-out hover:bg-indigo-600 mr-6 shadow-lg shadow-indigo-500/50 dark:shadow-indigo-800/50 rounded-lg'>
                     📝 Lihat Blog
                 </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,16 @@ import Seo from '../components/seo'
 import { postFormat } from '../lib/post-format'
 
 const IndexPage = ({ data, pageContext }) => {
-  const { NotionPosts, MarkdownPosts } = data
+  const { NotionPosts, MarkdownPosts, NotionWriteups, MarkdownWriteups } = data
 
   const articles = useMemo(
     () => postFormat(MarkdownPosts.nodes, NotionPosts.nodes),
     [MarkdownPosts.nodes, NotionPosts.nodes]
+  )
+  
+  const writeups = useMemo(
+    () => postFormat(MarkdownWriteups.nodes, NotionWriteups.nodes),
+    [MarkdownWriteups.nodes, NotionWriteups.nodes]
   )
 
   return (
@@ -28,6 +33,11 @@ const IndexPage = ({ data, pageContext }) => {
       <h1 className="text-3xl my-10 pt-10 font-semibold">Artikel terbaru</h1>
       <div className="mb-6">
         <Posts data={articles.slice(0,5)} />
+      </div>
+      
+      <h1 className="text-3xl my-10 pt-10 font-semibold">Writeups</h1>
+      <div className="mb-6">
+        <Posts data={writeups.slice(0,5)} />
       </div>
     </Layout>
   )
@@ -85,6 +95,70 @@ query IndexQuery {
     limit: 5
     sort: { fields: [frontmatter___date], order: DESC }
     filter: { fileAbsolutePath: { regex: "/content/posts/" } }
+  ) {
+    nodes {
+      id
+      frontmatter {
+        title
+        date(formatString: "DD MMMM YYYY", locale: "id-ID")
+        description
+      }
+      fields {
+        slug
+      }
+    }
+  }
+
+  NotionWriteups: allNotion(
+    filter: {properties: {type: {value: {name: {eq: "Writeup"}}}, status: {value: {name: {eq: "Posted"}}}}}
+    limit: 5
+    sort: {fields: properties___date___value___start, order: DESC}
+  ) {
+    nodes {
+      id
+      title
+      raw {
+        icon {
+          type
+          remoteImage {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          external {
+            url
+          }
+          emoji
+        }
+        properties {
+          Name {
+            id
+          }
+          date {
+            date {
+              start(locale: "id-ID", formatString: "DD MMMM YYYY")
+            }
+          }
+          slug {
+            rich_text
+          }
+          tags {
+            multi_select {
+              name
+            }
+          }
+          description {
+            rich_text
+          }
+        }
+      }
+    }
+  }
+  
+  MarkdownWriteups: allMarkdownRemark(
+    limit: 5
+    sort: { fields: [frontmatter___date], order: DESC }
+    filter: { fileAbsolutePath: { regex: "/content/writeups/" } }
   ) {
     nodes {
       id
