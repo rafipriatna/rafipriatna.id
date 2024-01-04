319516083fa831132e44eab9703c8c02bd92d263
Rafi Priatna K <rafipriatnak@gmail.com>
2024-01-05 00:24:58 +0700

    Update anchor link styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,10 @@ module.exports = {
           {
             resolve: `gatsby-remark-autolink-headers`,
             options: {
-              isIconAfterHeader: true,
+              icon: `<svg aria-hidden="true" height="24" version="1.1" viewBox="0 0 24 24" width="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path fill-rule="evenodd" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"></path></svg>`,
+              className: `anchor`,
+              maintainCase: true,
+              removeAccents: true,
             },
           },
           {
