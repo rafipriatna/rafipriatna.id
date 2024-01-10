bae2edae89553e7c80ba001ffe23404ca4b057b0
Rafi Priatna K <rafipriatnak@gmail.com>
2024-01-10 13:49:27 +0700

    Change className

--- src/components/author-card.js
+++ src/components/author-card.js
@@ -3,16 +3,16 @@ import { StaticImage } from "gatsby-plugin-image";
 
 export default function AuthorCard({ articleDate }) {
   return (
-    <div class="flex max-w-none items-center gap-4">
+    <div className="flex max-w-none items-center gap-4">
       <StaticImage
         src="../images/me.png"
         alt="8bit me wkwkw"
         className="rounded-full bg-indigo-500 dark:bg-indigo-800 w-10 h-10"
       />
-      <div class="leading-tight">
-        <p class="font-medium text-primary">Rafi Priatna Kasbiantoro</p>
-        <p class="text-gray-500">
-          <time datetime={articleDate}>{articleDate}</time>
+      <div className="leading-tight">
+        <p className="font-medium text-primary">Rafi Priatna Kasbiantoro</p>
+        <p className="text-gray-500">
+          <time dateTime={articleDate}>{articleDate}</time>
         </p>
       </div>
     </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -60,7 +60,7 @@ export default class Footer extends React.Component {
                                 className='ml-6 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-100'
                             >
                                 <span className='sr-only'>Twitter</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
                                     <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
                                 </svg>
                             </a>
