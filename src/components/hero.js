7613f2bfa6af1cad86d4ae346093b4602ae291ed
Rafi Priatna K <rafipriatnak@gmail.com>
2024-01-23 11:24:43 +0700

    New Hero design

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,33 +1,43 @@
-import React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Hero() {
-    return (
-        <div className="my-10 lg:mt-20 mb-10 bg-indigo-500 rounded-lg p-5 text-white bg-opacity-60 dark:bg-opacity-40 border-4 border-indigo-800 shadow-lg backdrop-blur">
-            <div className="flex flex-row items-center">
-                <div className="w-full lg:w-4/6">
-                    <h1 className="text-4xl font-semibold leading-tight md:leading-normal">
-                        Hei, saya Rafi!
-                    </h1>
-                    <p className="text-xl leading-relaxed md:order-1 my-4">
-                        Selamat datang di kebun digital saya.{" "}
-                        <span role="img" aria-label="tanaman">
-                            🌱
-                        </span>
-                        <span className="block mt-2">
-                            Saya seorang Offensive Cyber Security Engineer. Saya
-                            menulis artikel di sini selain untuk
-                            bersenang-senang, juga untuk catatan belajar saya.
-                        </span>
-                    </p>
-                </div>
-                <div className="w-48 h-48 relative hidden lg:block ml-auto hero">
-                    <StaticImage
-                        src='../images/me_transparent_big.png'
-                        alt='8bit me wkwkw'
-                    />
-                </div>
-            </div>
+  return (
+    <div className="my-10 lg:mt-20 mb-10 break-words tracking-wide leading-loose hero">
+      <div className="flex flex-row items-center">
+        <div className="w-full lg:w-4/6">
+          <h1 className="text-4xl font-semibold">Hei, saya Rafi!</h1>
+          <div className="text-lg leading-relaxed md:order-1 mt-8">
+            <p>
+              Saya seorang Offensive Cyber Security Engineer yang sehari-hari
+              melakukan <i>penetration testing</i> dan{" "}
+              <i>vulnerability assessment</i>.
+            </p>
+          </div>
+          <a
+            href="https://www.credential.net/b216417a-3265-4448-8913-f6a7d9122be1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <StaticImage
+              className="mt-5"
+              placeholder="blurred"
+              width={40}
+              height={40}
+              src="../images/OSCP.png"
+              alt="OffSec Certified Professional"
+              title="OffSec Certified Professional"
+            />
+          </a>
         </div>
-    )
+        <div className="w-48 h-48 relative hidden lg:block ml-auto">
+          <StaticImage
+            placeholder="blurred"
+            src="../images/me_transparent_big.png"
+            alt="8bit me wkwkw"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
